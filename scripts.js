715ded493f0a4d11d41415e4870916c18a1cdbcf
Hennie <henkovermeulen746@gmail.com>
2026-04-17 19:59:36 +0200

    script 1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,43 @@
+let issues = JSON.parse(localStorage.getItem("issues")) || [];
+localStorage.setItem("issues", JSON.stringify(issues));
 
+if (issues.length === 0) {
+    issues = [
+        {
+            id: 1,
+            summary: "Example Bug",
+            status: "open",
+            priority: "high"
+        }
+    ];
+    localStorage.setItem("issues", JSON.stringify(issues));
+}
+function showBug() {
+
+};
+function saveBug() {
+
+};
+function saveBug() {
+    let newBug ={id: Date.now(),
+    summary: document.getElementById("summary").value,
+    description: document.getElementById("description").value,
+    identifiedBy: document.getElementById("person").value,
+    project: document.getElementById("project").value,
+    assignedTo: document.getElementById("assignedTo").value,
+    priority: document.getElementById("priority").value,
+    status: "open",
+    dateIdentified: new Date().toISOString(),
+    targetDate: document.getElementById("targetDate").value,
+    resolvedDate: null,
+    resolutionSummary: ""
+    };
+    issues.push(newBug);
+    localStorage.setItem("issues", JSON.stringify(issues));
+
+    displayBugs();
+}
+function displayBugsSum() {};
+
+function displayDetail() {};
+function saveEdit() {};
